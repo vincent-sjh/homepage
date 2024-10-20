bb48ff961caca3bedf5f3af0c8051b163bc0d6ff
vincent-sjh <songjh22@mails.tsinghua.edu.cn>
2024-10-20 21:05:51 +0800

    size

--- layout/more/more.js
+++ layout/more/more.js
@@ -1,7 +1,7 @@
 let currentImageIndex = 0;
 const images = document.querySelectorAll(".imageWithCaption .slideshow img");
 const caption = document.querySelector(".imageWithCaption .caption");
-const imageDescriptions = ["描述 LA", "描述 Boston", "描述 Chicago"]; // 依次添加更多描述
+const imageDescriptions = ["描述 LA", "描述 Boston", "描述 Chicago", "描述沈阳"]; // 依次添加更多描述
 
 function changeImage() {
   images[currentImageIndex].style.display = "none"; // 隐藏当前图片
